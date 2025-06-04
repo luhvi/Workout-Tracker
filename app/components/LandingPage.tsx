77c2d1417c519bd4d0834395aabac77e304ca747
luhvi <luhvilive@gmail.com>
2025-06-04 11:57:40 +0200

    fixed a few more more bugs

---
+++ app/components/LandingPage.tsx
@@ -0,0 +1,15 @@
+export const LandingPageMain = () => {
+  return (
+    <div className="mt-60 flex h-full w-[100vw] flex-col text-center">
+      <h1 className="mb-2 px-24 font-[family-name:var(--font-geist-mono)] text-3xl font-bold md:text-4xl">
+        Workout Tracker
+      </h1>
+      <p className="md:text-md px-24 font-[family-name:var(--font-geist-mono)] text-sm">
+        Welcome to my Workout Tracker. This app is designed for those who wish
+        to take their fitness journey to the next level. It helps track your
+        lifts alongside your bodyweight to make sure you&apos;re always
+        progressing!
+      </p>
+    </div>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { useDropdown } from "./DropdownContext";
 
 import { Navbar } from "./ui/Navbar";
 import { DropdownNav } from "./ui/DropdownNav";
+import { LandingPageMain } from "./components/LandingPage";
 
 export default function Home() {
   const { showDropdown } = useDropdown();
@@ -16,19 +17,3 @@ export default function Home() {
     </div>
   );
 }
-
-export const LandingPageMain = () => {
-  return (
-    <div className="mt-60 flex h-full w-[100vw] flex-col text-center">
-      <h1 className="mb-2 px-24 font-[family-name:var(--font-geist-mono)] text-3xl font-bold md:text-4xl">
-        Workout Tracker
-      </h1>
-      <p className="md:text-md px-24 font-[family-name:var(--font-geist-mono)] text-sm">
-        Welcome to my Workout Tracker. This app is designed for those who wish
-        to take their fitness journey to the next level. It helps track your
-        lifts alongside your bodyweight to make sure you&apos;re always
-        progressing!
-      </p>
-    </div>
-  );
-};
